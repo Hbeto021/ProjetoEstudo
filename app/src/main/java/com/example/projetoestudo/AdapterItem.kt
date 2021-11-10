eaac4abb4b73d67dbc2bcba7e48841d432196266
Hbeto021 <f0129605@PM759338.local>
2021-11-09 23:43:16 -0300

    adiciona comentários

--- app/src/main/java/com/example/projetoestudo/AdapterItem.kt
+++ app/src/main/java/com/example/projetoestudo/AdapterItem.kt
@@ -10,6 +10,9 @@ import androidx.recyclerview.widget.RecyclerView
 class AdapterItem(val listaItem: ArrayList<Livro>,val context: Context): RecyclerView.Adapter<AdapterItem.itemViewHolder>(){
 
 
+//    como a classe ItemViewHolder é interna do adapter, coloca ela como inner class,
+//    assim ninguem instancia ela de outro lugar (Lembra de sempre iniciar nome de classe
+//    com letra maiscula
     class itemViewHolder(view: View): RecyclerView.ViewHolder(view){
         val titulo = view.findViewById(R.id.txtTitulo) as TextView
         val data = view.findViewById(R.id.txt_data_publicacao) as TextView
@@ -18,11 +21,13 @@ class AdapterItem(val listaItem: ArrayList<Livro>,val context: Context): Recycle
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): itemViewHolder {
         //parent?.context... varifica se o contexto esta null
+        //não precisa verificar se o contexto esta nulo, context nesse caso é non null
         val viewLayoutInfla = LayoutInflater.from(parent?.context).inflate(R.layout.adapter_item,parent,false)
         return itemViewHolder(viewLayoutInfla)
     }
 
     override fun onBindViewHolder(holder: itemViewHolder, position: Int) {
+        //não precisa do modificador ? pois as varivaies são non null
         holder.titulo?.text = listaItem[position].titulo
         holder.data?.text = listaItem[position].dataPublicacao
         holder.autor?.text = listaItem[position].autor
